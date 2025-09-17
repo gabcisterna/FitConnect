b2e8339313e207be9d9d7b95ec00193ea298ec0d
GabCisterna0 <gabriel.cisterna@mi.unc.edu.ar>
2025-09-17 14:06:26 +0000

    feat(scheduling): Room y ClassType sin instructores; duración fija 60min

--- scheduling/admin.py
+++ scheduling/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
+from .models import Room, ClassType
 
 # Register your models here.
+@admin.register(Room)
+class RoomAdmin(admin.ModelAdmin):
+    list_display = ("name", "capacity")
+    search_fields = ("name",)
+
+@admin.register(ClassType)
+class ClassTypeAdmin(admin.ModelAdmin):
+    list_display = ("name", "duration_min", "default_capacity")
+    readonly_fields = ("duration_min",)
+    search_fields = ("name",)
--- scheduling/models.py
+++ scheduling/models.py
@@ -1,3 +1,24 @@
 from django.db import models
 
 # Create your models here.
+class Room(models.Model):
+    name = models.CharField(max_length=80, unique=True)
+    capacity = models.PositiveIntegerField(default=20)
+
+    def __str__(self):
+        return f"{self.name} (cap: {self.capacity})"
+
+
+class ClassType(models.Model):
+    """
+    Tipo de clase (ej: Funcional, Spinning, Yoga).
+    Todas las clases duran 60 minutos (fijo).
+    """
+    name = models.CharField(max_length=80, unique=True)
+    # Fijamos duración a 60 y no editable para hacerla inmutable desde el admin
+    duration_min = models.PositiveIntegerField(default=60, editable=False)
+    default_capacity = models.PositiveIntegerField(default=20)
+
+    def __str__(self):
+        return self.name
+
